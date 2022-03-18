76e7048f8fc1e585040cac7923ad1caa44af15dd
naufal22hadi <naufal22hadi@gmail.com>
2022-03-18 15:26:21 +0700

    adding style to button and textview.

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.project1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val text : TextView get() = findViewById(R.id.lionParcelTextView)
+    private val button : Button get() = findViewById(R.id.lionParcelButton)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text.text = "Naufal Hadi"
+        button.text = "Press me!"
     }
 }
